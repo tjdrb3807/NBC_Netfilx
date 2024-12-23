4eb2ca350f235676913f90dc69522b9f96ea86e8
Jeon_Seong_gyu <junsung3807@naver.com>
2024-12-23 14:53:32 +0900

    🎉 Begin a project

---
+++ NBC_Netfilx/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  NBC_Netfilx
+//
+//  Created by 전성규 on 12/23/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
